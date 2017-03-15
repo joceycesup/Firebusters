6bb32a15ce5ea7d1a9ab78f16dd6b9049ea51f9a
Thibault Baron <thibault.baron56@gmail.com>
2017-03-15 15:47:25 +0100

    WWise deployments and gitignore correction

---
+++ Assets/Scripts/MarionetteController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarionetteController : MonoBehaviour {
+	public float maxXAngle = 25.0f;
+	public float maxZAngle = 25.0f;
+	public float yAngle = 0.0f;
+	public float turnRate = 45.0f;
+	
+	void Update () {
+		transform.rotation = Quaternion.Euler (Input.GetAxis ("Vertical") * maxXAngle, 0.0f, Input.GetAxis ("Horizontal") * -maxZAngle);
+	}
+}
