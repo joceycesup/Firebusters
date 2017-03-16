129ee93df15a32dd094c13aa6e96a1ffc03808a1
Thibault Baron <thibault.baron56@gmail.com>
2017-03-16 04:21:39 +0100

    Added old prototype and simulated ragdoll walk using oscillating rotations on the marionette controller

--- Assets/Scripts/MarionetteController.cs
+++ Assets/Scripts/MarionetteController.cs
@@ -7,10 +7,50 @@ public class MarionetteController : MonoBehaviour {
 	public float maxZAngle = 25.0f;
 	public float yAngle = 0.0f;
 	public float turnRate = 45.0f;
-	
+	public float speed = 4.0f;
+
+	public float maxYAngle = 25.0f;
+	private float period = 1.0f;
+
+	[Range (0.01f, 1.0f)]
+	public float timeScale = 1.0f;
+
 	void Update () {
-		yAngle -= turnRate * Time.deltaTime * Input.GetAxis ("HorizontalR");
-		transform.rotation = Quaternion.Euler (Input.GetAxis ("VerticalL") * maxXAngle, 0.0f, Input.GetAxis ("HorizontalL") * -maxZAngle);
+		Time.timeScale = timeScale;
+		/*
+		transform.Translate (Vector3.up * speed * Time.deltaTime * Input.GetAxis ("Depth"), Space.World);
+		yAngle += turnRate * Time.deltaTime * Input.GetAxis ("HorizontalR");
+		transform.rotation = Quaternion.Euler (0f, 0f, 0f);
+
+		Vector3 rotation = Camera.main.transform.right;
+		rotation.y = 0f;
+		rotation = Vector3.Normalize (rotation) * Input.GetAxis ("VerticalL") * maxXAngle;
+		transform.Rotate (rotation, Space.World);
+
+		rotation = Camera.main.transform.forward;
+		rotation.y = 0f;
+		rotation = Vector3.Normalize (rotation) * Input.GetAxis ("HorizontalL") * -maxZAngle;
+		transform.Rotate (rotation, Space.World);
+		//Quaternion.Euler (Input.GetAxis ("VerticalL") * maxXAngle, 0.0f, Input.GetAxis ("HorizontalL") * -maxZAngle);
+		transform.Rotate (Vector3.up * yAngle, Space.Self);/*/
+		float factor = Mathf.Sin (Time.time * period * Mathf.PI);
+		yAngle = maxYAngle * factor;
+
+
+		transform.Translate (Vector3.up * speed * Time.deltaTime * Input.GetAxis ("Depth"), Space.World);
+		transform.rotation = Quaternion.Euler (0f, 0f, 0f);
+
+		Vector3 rotation = Camera.main.transform.right;
+		rotation.y = 0f;
+		rotation = Vector3.Normalize (rotation) * (1f - Mathf.Cos (yAngle * 2f*Mathf.Deg2Rad)) * maxXAngle;
+		transform.Rotate (rotation, Space.World);
+
+		rotation = Camera.main.transform.forward;
+		rotation.y = 0f;
+		rotation = Vector3.Normalize (rotation) * Mathf.Sin (yAngle * Mathf.Deg2Rad) * maxZAngle;
+		transform.Rotate (rotation, Space.World);
+
 		transform.Rotate (Vector3.up * yAngle, Space.Self);
+		//*/
 	}
 }
---
+++ Assets/Scripts/PuppetString.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (LineRenderer))]
+public class PuppetString : MonoBehaviour {
+	private LineRenderer lr;
+	public GameObject attachedObject;
+	private Rigidbody rb;
+	public float speed = 4;
+	public float length {
+		get;
+		private set;
+	}
+	[SerializeField]
+	[Range(0f, 100000f)]
+	float springConstant = 10000f;
+	[SerializeField]
+	[Range(0f, 100000f)]
+	float viscousDampingCoefficient = 10000f;
+
+	private float relaxedLength;
+	private Vector3 lastPos;
+
+	//public string controllerExtension = "L";
+
+	void Start () {
+		if (!attachedObject)
+			return;
+		rb = attachedObject.GetComponent<Rigidbody> ();
+		lr = GetComponent<LineRenderer> ();
+		relaxedLength = Vector3.Magnitude (transform.position - attachedObject.transform.position);
+	}
+
+	void Update () {
+		if (!attachedObject)
+			return;
+		lr.SetPosition (0, transform.position);
+		lr.SetPosition (1, attachedObject.transform.position);
+	}
+	
+	void FixedUpdate () {
+		if (rb) {
+			//rb.velocity = Vector3.zero;
+			/*
+			acceleration = (rb.velocity - lastVelocity) / Time.fixedDeltaTime;
+			lastVelocity = rb.velocity;//*/
+			float magnitude = Vector3.Magnitude (transform.position - attachedObject.transform.position);
+			if (magnitude > relaxedLength) {
+				//spring
+				rb.AddForce (Vector3.Normalize (transform.position - attachedObject.transform.position) * Time.fixedDeltaTime * springConstant * (magnitude - relaxedLength), ForceMode.Force);
+				//dampening
+				if (viscousDampingCoefficient > 0f) {
+					rb.AddForce (-viscousDampingCoefficient * Vector3.Project (attachedObject.transform.position - lastPos, transform.position - attachedObject.transform.position) * Time.fixedDeltaTime, ForceMode.Force);
+				}
+				lr.SetColors (Color.red, Color.red);
+			} else {
+				lr.SetColors (Color.green, Color.green);
+			}
+		}
+		lastPos = attachedObject.transform.position;
+	}
+}
