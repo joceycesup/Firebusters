551f523d0744231f32ebcc7f883ad73617b2c5a9
BARON Thibault <Thibault.baron@formation.enjmin.fr>
2017-03-28 19:13:01 +0200

    Separated graphical functions in input analyzer

--- Assets/Scripts/InputAnalyzer.cs
+++ Assets/Scripts/InputAnalyzer.cs
@@ -9,9 +9,10 @@ public class InputAnalyzer : MonoBehaviour {
 	public Text output;
 	[Range (128, 1024)]
 	private int size = 512;
-	public int analyzedValues = 8;
+	private int analyzedValues = 8;
 	private int valuesOffset = 0;
 	private float[] values;
+	private float[] slopes;
 	private float[] wave_amplitudes;
 	private float[] wave_periods;
 	private int waves = 8;
@@ -20,9 +21,20 @@ public class InputAnalyzer : MonoBehaviour {
 	private float lastSpikeTime = 0.0f;
 	private float lastDelta = 0.0f;
 
+	//------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------
+	private float delayThreshold = 1.0f;
+	private float amplitudeThreshold = 0.2f;
+	//------------------------------------------------------------------------------------------------
+	//------------------------------------------------------------------------------------------------
+
+	public Transform controller;
+
 	void Start () {
+		Debug.Log (Time.fixedDeltaTime);
 		size = Mathf.ClosestPowerOfTwo (size);
 		values = new float[size];
+		slopes = new float[size];
 		wave_amplitudes = new float[waves];
 		wave_periods = new float[waves];
 		ren = GetComponent<MeshRenderer> ();
@@ -42,33 +54,39 @@ public class InputAnalyzer : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		values[valuesOffset] = Input.GetAxis ("HorizontalL");
+		if (controller)
+			values[valuesOffset] = controller.localRotation.eulerAngles.z;
+		else
+			values[valuesOffset] = Input.GetAxis ("HorizontalL");
 		int analyzeOffset = valuesOffset - analyzedValues;
 		if (analyzeOffset < 0)
 			analyzeOffset += size;
+		//------------------------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		//------------------------------------------------------------------------------------------------
 		float delta = 0.0f;
-		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
-		//make a lastDeltas
-		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
+		float absDelta = 0.0f;
 		for (int i = 0; i < analyzedValues; ++i, ++analyzeOffset) {
 			if (analyzeOffset + 1 >= size) {
 				analyzeOffset = 0;
 				delta += (values[0] - values[size - 1]);
+				absDelta += Mathf.Abs (values[0] - values[size - 1]);
 			}
 			else {
 				delta += (values[analyzeOffset + 1] - values[analyzeOffset]);
+				absDelta += Mathf.Abs (values[analyzeOffset + 1] - values[analyzeOffset]);
 			}
 		}
 		delta /= analyzedValues;
-		if (delta * lastDelta < 0.0f) {
+		absDelta /= analyzedValues;
+		if (delta * lastDelta < 0.0f) {/*
+			if ((Time.time - lastSpikeTime) >= delayThreshold && absDelta < amplitudeThreshold)
+				lastSpikeTime = Time.time;//*/
 			output.text = (Time.time - lastSpikeTime).ToString ();
 			wave_periods[wavesOffset] = Time.time - lastSpikeTime;
 			lastSpikeTime = Time.time;
@@ -91,40 +109,45 @@ public class InputAnalyzer : MonoBehaviour {
 		//------------------------------------------------------------------------------------------------
 		//------------------------------------------------------------------------------------------------
 		//------------------------------------------------------------------------------------------------
-		//------------------------------------------------------------------------------------------------
 		if (ren) {
-			/*
-			Color[] colors = new Color[3];
-			colors[0] = Color.red;
-			colors[1] = Color.green;
-			colors[2] = Color.blue;
-			int mipCount = Mathf.Min (3, tex.mipmapCount);
-			// tint each mip level
-			for (int mip = 0; mip < mipCount; ++mip) {
-				Color[] cols = tex.GetPixels (mip);
-				for (int i = 0; i < cols.Length; ++i) {
-					cols[i] = colors[mip];
-				}
-				tex.SetPixels (cols, mip);
-			}/*/
-			drawLine (valuesOffset, 0, valuesOffset, size, Color.black);
-			if (valuesOffset < size - 1)
-				drawLine (valuesOffset + 1, 0, valuesOffset + 1, size, Color.green);
-
-			int value = size / 2 - (int) (values[valuesOffset] * size / 2);
-			if (valuesOffset > 0) {
-				int lastValue = size / 2 - (int) (values[valuesOffset - 1] * size / 2);
-				drawLine (valuesOffset - 1, lastValue, valuesOffset, value, Color.white);
-			}
-			else {
-				tex.SetPixel (valuesOffset, value, Color.white);
-			}
+			drawGraph (ref values, Color.white);
+			drawGraph (ref slopes, Color.yellow);
 			tex.SetPixel (valuesOffset, size / 2, Color.red);
 			tex.Apply (false);
 		}
+		//------------------------------------------------------------------------------------------------
 		valuesOffset++;
 		if (valuesOffset >= size)
 			valuesOffset = 0;
+	}
+
+	private void drawGraph (ref float[] array, Color color) {
+		/*
+		Color[] colors = new Color[3];
+		colors[0] = Color.red;
+		colors[1] = Color.green;
+		colors[2] = Color.blue;
+		int mipCount = Mathf.Min (3, tex.mipmapCount);
+		// tint each mip level
+		for (int mip = 0; mip < mipCount; ++mip) {
+			Color[] cols = tex.GetPixels (mip);
+			for (int i = 0; i < cols.Length; ++i) {
+				cols[i] = colors[mip];
+			}
+			tex.SetPixels (cols, mip);
+		}/*/
+		drawLine (valuesOffset, 0, valuesOffset, size, Color.black);
+		if (valuesOffset < size - 1)
+			drawLine (valuesOffset + 1, 0, valuesOffset + 1, size, Color.green);
+
+		int value = size / 2 - (int) (array[valuesOffset] * size / 2);
+		if (valuesOffset > 0) {
+			int lastValue = size / 2 - (int) (array[valuesOffset - 1] * size / 2);
+			drawLine (valuesOffset - 1, lastValue, valuesOffset, value, color);
+		}
+		else {
+			tex.SetPixel (valuesOffset, value, color);
+		}
 	}
 
 	private void drawLine (int x0, int y0, int x1, int y1, Color color) {
