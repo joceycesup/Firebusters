26e94e8e76d4ef4cb33994871407c84c7b2eefa4
Peggy-p <plms@laposte.net>
2017-04-27 16:42:28 +0200

    intégration assets premier étage 1

---
+++ Assets/Scripts/Utility/FBPlacePrefab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBPlacePrefab : MonoBehaviour {
+	public GameObject prefab;
+
+	void Awake () {
+		GameObject go = Instantiate (prefab, transform.position, transform.rotation);
+		go.transform.parent = transform.parent;
+		Destroy (gameObject);
+	}
+}
