74159940b1c74ff3c46a1ecf697e5051233270f4
Thibault Baron <thibault.baron56@gmail.com>
2017-05-30 01:37:38 +0200

    Extincteur 2.0

--- Assets/Scripts/Interactions/FBExtinguisher.cs
+++ Assets/Scripts/Interactions/FBExtinguisher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//[ExecuteInEditMode]
 public class FBExtinguisher : FBTool {
 	private ParticleSystem ps;
 	private List<GameObject> fires;
@@ -10,10 +11,14 @@ public class FBExtinguisher : FBTool {
 	public float force = 50.0f;
 	public ForceMode forceMode = ForceMode.Impulse;
 
+	[Range (1.0f, 20.0f)]
+	public float raycastDivisions = 10.0f;
+
 	void Awake () {
 		ps = gameObject.GetComponent<ParticleSystem> ();
 		ps.Stop ();
 		fires = new List<GameObject> ();
+		raycastDivisions = Mathf.Round (raycastDivisions) - 0.01f;
 	}
 
 	public override void Enable () {
@@ -24,8 +29,12 @@ public class FBExtinguisher : FBTool {
 		ps.Stop ();
 	}
 
-	void FixedUpdate () {//*
-		if (ps.isPlaying) {
+	void Update () {
+#if DEBUG_ENABLED
+		if (!Application.isPlaying)
+			raycastDivisions = Mathf.Round (raycastDivisions) - 0.01f;
+#endif
+		if (ps.isPlaying) {/*
 			Debug.DrawRay (transform.position, transform.forward * raycastDistance, Color.red);
 			Ray ray = new Ray (transform.position, transform.forward);
 			float maxDistance = float.MaxValue;
@@ -39,7 +48,7 @@ public class FBExtinguisher : FBTool {
 				else if (hit.rigidbody && !hit.rigidbody.isKinematic) {
 					hit.rigidbody.AddForce (transform.forward * force * forceDecrease.Evaluate (hit.distance / raycastDistance), forceMode);
 				}
-			}//*/
+			}
 			hits = Physics.RaycastAll (ray, raycastDistance, 1 << 12); // 9 : VerticalObstacle, 1 : Default, 12 : Fire
 			foreach (RaycastHit hit in hits) {
 				float delay = hit.distance / ps.main.startSpeedMultiplier;
@@ -49,6 +58,63 @@ public class FBExtinguisher : FBTool {
 					}
 				}
 			}//*/
+
+			Ray ray = new Ray ();
+			float maxDistance = float.MaxValue;
+			RaycastHit[] hits;
+
+			float raycastDelta = ps.main.startLifetimeMultiplier / raycastDivisions;
+			float distance = 0.0f;
+			float totalSpeed = 0.0f;
+
+			//Debug.Log (ps.main.startSpeedMultiplier + " ; " + ps.main.startLifetimeMultiplier + " ; " + ps.main.gravityModifierMultiplier);
+			Vector3 speed = transform.forward * ps.main.startSpeedMultiplier;
+			Vector3 position = transform.position;
+			float time = 0.0f;
+			float i = 1.0f;
+			while (time < ps.main.startLifetimeMultiplier) {
+#if DEBUG_ENABLED
+				Vector3 oldDirection = speed;
+#endif
+				totalSpeed += speed.magnitude;
+				speed += ps.main.gravityModifierMultiplier * Physics.gravity * raycastDelta;
+
+				maxDistance = float.MaxValue;
+				ray = new Ray (position, speed);
+				hits = Physics.RaycastAll (ray, speed.magnitude * raycastDelta, 0x1301); // 8 : Walkable, 9 : VerticalObstacle, 1 : Default, 12 : Fire
+
+				foreach (RaycastHit hit in hits) {
+					if (hit.collider.gameObject.layer == 9 || hit.collider.gameObject.layer == 8) {
+						time = ps.main.startLifetimeMultiplier;
+						if (hit.distance < maxDistance)
+							maxDistance = hit.distance;
+					}
+					else if (hit.rigidbody && !hit.rigidbody.isKinematic) {
+						hit.rigidbody.AddForce (transform.forward * force * forceDecrease.Evaluate (((distance + hit.distance) / (totalSpeed / i)) / ps.main.startLifetimeMultiplier), forceMode);
+					}
+				}
+				foreach (RaycastHit hit in hits) {
+					if (hit.collider.gameObject.layer == 12) {
+						float delay = (distance + hit.distance) / (totalSpeed / i);
+						if (delay < ps.main.startLifetimeMultiplier && hit.distance < maxDistance) {
+							if (!fires.Contains (hit.collider.gameObject)) {
+								StartCoroutine (PutOutFire (hit.collider.gameObject, delay));
+							}
+						}
+					}
+				}
+
+
+#if DEBUG_ENABLED
+				Debug.DrawRay (position, speed * raycastDelta, Color.red);
+#endif
+				distance += (position - (position += speed * raycastDelta)).magnitude;
+#if DEBUG_ENABLED
+				Debug.DrawLine (position, position + (speed.normalized - oldDirection.normalized).normalized / -5.0f, new Color (1.0f, 0.5f, 0.0f));
+#endif
+				time += raycastDelta;
+				i += 1.0f;
+			}
 		}
 
 		//ps.startLifetime
