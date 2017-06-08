d38925c8fd606c57d7797853d00482840e3926b8
Thibault Baron <thibault.baron56@gmail.com>
2017-06-08 11:36:24 +0200

    Menu 1

---
+++ Assets/Scripts/Utility/FBMenuHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FBMenuHandler : MonoBehaviour {
+	public Canvas canvas;
+	public RectTransform creditsPanel;
+	public RectTransform menuPanel;
+	public Camera cam;
+	public Transform menuCamPosition;
+	public Transform creditsCamPosition;
+	public float slideDelay = 1.0f;
+
+	void Start () {
+		creditsPanel.pivot = new Vector2 (0.5f, -1.0f);
+		cam.transform.position = menuCamPosition.position;
+		cam.transform.rotation = menuCamPosition.rotation;
+	}
+
+	public void ShowCredits () {
+		StartCoroutine (SlidePanel (creditsPanel, new Vector2 (0.5f, -1.0f), new Vector2 (0.5f, 0.0f)));
+		StartCoroutine (SlidePanel (menuPanel, new Vector2 (0.5f, 1.0f), new Vector2 (0.5f, 2.0f)));
+		StartCoroutine (MoveCamera (menuCamPosition, creditsCamPosition));
+	}
+
+	public void ShowMenu () {
+		StartCoroutine (SlidePanel (creditsPanel, new Vector2 (0.5f, 0.0f), new Vector2 (0.5f, -1.0f)));
+		StartCoroutine (SlidePanel (menuPanel, new Vector2 (0.5f, 2.0f), new Vector2 (0.5f, 1.0f)));
+		StartCoroutine (MoveCamera (creditsCamPosition, menuCamPosition));
+	}
+
+	public void ExitGame () {
+		Application.Quit ();
+	}
+
+	public IEnumerator SlidePanel (RectTransform target, Vector2 start, Vector2 end) {
+		float startTime = Time.time;
+		while (Time.time < startTime + slideDelay) {
+			target.pivot = Vector2.Lerp (start, end, (Time.time - startTime) / slideDelay);
+			yield return null;
+		}
+		target.pivot = end;
+	}
+
+	public IEnumerator MoveCamera (Transform start, Transform end) {
+		float startTime = Time.time;
+		while (Time.time < startTime + slideDelay) {
+			cam.transform.position = Vector3.Lerp (start.position, end.position, (Time.time - startTime) / slideDelay);
+			cam.transform.rotation = Quaternion.Lerp (start.rotation, end.rotation, (Time.time - startTime) / slideDelay);
+			yield return null;
+		}
+		cam.transform.position = end.position;
+		cam.transform.rotation = end.rotation;
+	}
+}
---
+++ Assets/Scripts/Utility/FBRotateName.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBRotateName : MonoBehaviour {
+	public Transform target;
+	public float distance = 4.0f;
+
+	void Start () {
+		transform.position = target.position + (transform.position - target.position).normalized * distance;
+		transform.rotation = Quaternion.LookRotation (target.position - transform.position, target.up) * Quaternion.Euler (-90.0f, 0.0f, 0.0f);
+	}
+}
